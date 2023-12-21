19b965131d8b0d1a08092671feb90edecfaf326d
Tony Garcia <tonygarcia@Tonys-Air.attlocal.net>
2023-12-20 16:14:30 -0800

    anotha one

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 const pg = require('pg')
-
 const client = new pg.Client('postgres://localhost/pet_finder')
-
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+
+app.use(cors())
 app.get('/api/pets', async (req, res, next) => {
     try {
         const SQL = `
            SELECT *
-           FROM things
+           FROM pets
         `
         const response = await client.query(SQL)
-        console.log(response)
+        console.log(response.rows)
+        res.send(response.rows)
 
     } catch (error) {
+        next(error)
 
     }
 })
 
-const init = async() => {
+const init = async () => {
     await client.connect()
     console.log("connected to database")
     const SQL = `
     DROP TABLE IF EXISTS pets;
     CREATE TABLE pets(
         id SERIAL PRIMARY KEY,
-        name VARCHAR(100)
+        name VARCHAR(100),
+        is_favorite BOOLEAN
     );
-    INSERT INTO pet (name) VALUES ('Tiny');
-    INSERT INTO pet (name) VALUES ('Smokey');
-    INSERT INTO pet (name) VALUES ('Max');
-    INSERT INTO pet (name) VALUES ('Blaze');
-    INSERT INTO pet (name) VALUES ('Milo');
+    INSERT INTO pets (name) VALUES ('Tiny');
+    INSERT INTO pets (name, is_favorite) VALUES ('Smokey', true);
+    INSERT INTO pets (name) VALUES ('Max');
+    INSERT INTO pets (name) VALUES ('Blaze');
+    INSERT INTO pets (name) VALUES ('Milo');
     `
     await client.query(SQL)
     console.log("table created")
